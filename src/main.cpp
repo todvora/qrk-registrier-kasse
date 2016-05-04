82aa935cf1fa5e405d86f2f40f9ff2bdf24edd41
ckvsoft <chris@ckvsoft.at>
2016-05-04 19:50:23 +0200

    Add QtInfoMsg to switch statement

--- src/main.cpp
+++ src/main.cpp
@@ -51,15 +51,18 @@ void QRKMessageHandler(QtMsgType type, const QMessageLogContext &, const QString
     case QtDebugMsg:
         txt = QString("%1 %2 Debug: %3").arg(QDateTime::currentDateTime().toString("yyyy.MM.dd hh:mm:ss.zzz")).arg(type).arg(str);
         break;
+    case QtInfoMsg:
+        txt = QString("%1 %2 Info: %3").arg(QDateTime::currentDateTime().toString("yyyy.MM.dd hh:mm:ss.zzz")).arg(type).arg(str);
+        break;
     case QtWarningMsg:
         txt = QString("%1 %2 Warning: %3").arg(QDateTime::currentDateTime().toString("yyyy.MM.dd hh:mm:ss.zzz")).arg(type).arg(str);
-    break;
+        break;
     case QtCriticalMsg:
         txt = QString("%1 %2 Critical: %3").arg(QDateTime::currentDateTime().toString("yyyy.MM.dd hh:mm:ss.zzz")).arg(type).arg(str);
-    break;
+        break;
     case QtFatalMsg:
         txt = QString("%1 %2 Fatal: %3").arg(QDateTime::currentDateTime().toString("yyyy.MM.dd hh:mm:ss.zzz")).arg(type).arg(str);
-    break;
+        break;
     }
     QFile outFile(qApp->applicationDirPath() + "/qrk.log");
     if (outFile.size() > 20000000) /*20 Mega*/
@@ -72,46 +75,46 @@ void QRKMessageHandler(QtMsgType type, const QMessageLogContext &, const QString
 
 void printHelp()
 {
-  printf("QRK understands:\n"
-         "-db ... open database defintion dialog\n"
-         "-f | --fullscreen ... work in fullscreen mode\n"
-         "-n | --noPrinter ... for tests, do not print to printer but to PDF\n"
-         "--help ... this text\n");
+    printf("QRK understands:\n"
+           "-db ... open database defintion dialog\n"
+           "-f | --fullscreen ... work in fullscreen mode\n"
+           "-n | --noPrinter ... for tests, do not print to printer but to PDF\n"
+           "--help ... this text\n");
 }
 
 //--------------------------------------------------------------------------------
 
 void sighandler(int /*sig*/)
 {
-  qApp->exit();
+    qApp->exit();
 }
 
 int main(int argc, char *argv[])
 {
-  QApplication app(argc, argv);
-  QApplication::setStyle(QStyleFactory::create("Fusion"));
+    QApplication app(argc, argv);
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
 
-  qInstallMessageHandler(QRKMessageHandler);
+    qInstallMessageHandler(QRKMessageHandler);
 
-  // Prerequisite for the Fervor updater
-  QApplication::setOrganizationName("ckvsoft");
-  QApplication::setOrganizationDomain("ckvsoft.at");
-  QApplication::setApplicationName("QRK");
-  QApplication::setApplicationVersion(QString("%1.%2").arg(QRK_VERSION_MAJOR).arg(QRK_VERSION_MINOR));
+    // Prerequisite for the Fervor updater
+    QApplication::setOrganizationName("ckvsoft");
+    QApplication::setOrganizationDomain("ckvsoft.at");
+    QApplication::setApplicationName("QRK");
+    QApplication::setApplicationVersion(QString("%1.%2").arg(QRK_VERSION_MAJOR).arg(QRK_VERSION_MINOR));
 
-  // Set feed URL before doing anything else
-  FvUpdater::sharedUpdater()->SetFeedURL("http://service.ckvsoft.at/swupdates/Appcast.xml");
-  FvUpdater::sharedUpdater()->setRequiredSslFingerPrint("c3b038cb348c7d06328579fb950a48eb");	// Optional
-  FvUpdater::sharedUpdater()->setHtAuthCredentials("swupdates", "updatepw");	// Optional
-  FvUpdater::sharedUpdater()->setSkipVersionAllowed(true);	// Optional
-  FvUpdater::sharedUpdater()->setRemindLaterAllowed(true);	// Optional
-  // Finish Up old Updates
-  FvUpdater::sharedUpdater()->finishUpdate();
+    // Set feed URL before doing anything else
+    FvUpdater::sharedUpdater()->SetFeedURL("http://service.ckvsoft.at/swupdates/Appcast.xml");
+    FvUpdater::sharedUpdater()->setRequiredSslFingerPrint("c3b038cb348c7d06328579fb950a48eb");	// Optional
+    FvUpdater::sharedUpdater()->setHtAuthCredentials("swupdates", "updatepw");	// Optional
+    FvUpdater::sharedUpdater()->setSkipVersionAllowed(true);	// Optional
+    FvUpdater::sharedUpdater()->setRemindLaterAllowed(true);	// Optional
+    // Finish Up old Updates
+    FvUpdater::sharedUpdater()->finishUpdate();
 
-  // Check for updates automatically
-  FvUpdater::sharedUpdater()->CheckForUpdatesSilent();
+    // Check for updates automatically
+    FvUpdater::sharedUpdater()->CheckForUpdatesSilent();
 
-  /*
+    /*
   QSharedMemory mem("QRK");
   if(!mem.create(1))
   {
@@ -120,99 +123,99 @@ int main(int argc, char *argv[])
   }
 */
 
-  QString locale = QLocale::system().name();
+    QString locale = QLocale::system().name();
 
-  QTranslator trans;
-  QString translationPath = QLibraryInfo::location(QLibraryInfo::TranslationsPath);
-  if ( trans.load(QLatin1String("qt_") + locale, translationPath) )
-    app.installTranslator(&trans);
+    QTranslator trans;
+    QString translationPath = QLibraryInfo::location(QLibraryInfo::TranslationsPath);
+    if ( trans.load(QLatin1String("qt_") + locale, translationPath) )
+        app.installTranslator(&trans);
 
-  // QRK translation file is searched in the application install dir (e.g. /opt/QRK)
-  // and also in /usr/share/QRK
-  QTranslator trans2;  // if current language is not de and translation not found, use english
-  if ( !locale.startsWith(QLatin1String("de")) &&
-       (trans2.load(QLatin1String("QRK_") + locale, QCoreApplication::applicationDirPath()) ||
-        trans2.load(QLatin1String("QRK_") + locale, QLatin1String("/usr/share/QRK")) ||
-        trans2.load(QLatin1String("QRK_en"), QCoreApplication::applicationDirPath()) ||
-        trans2.load(QLatin1String("QRK_en"), QLatin1String("/usr/share/QRK"))) )
-    app.installTranslator(&trans2);
+    // QRK translation file is searched in the application install dir (e.g. /opt/QRK)
+    // and also in /usr/share/QRK
+    QTranslator trans2;  // if current language is not de and translation not found, use english
+    if ( !locale.startsWith(QLatin1String("de")) &&
+         (trans2.load(QLatin1String("QRK_") + locale, QCoreApplication::applicationDirPath()) ||
+          trans2.load(QLatin1String("QRK_") + locale, QLatin1String("/usr/share/QRK")) ||
+          trans2.load(QLatin1String("QRK_en"), QCoreApplication::applicationDirPath()) ||
+          trans2.load(QLatin1String("QRK_en"), QLatin1String("/usr/share/QRK"))) )
+        app.installTranslator(&trans2);
 
-  QRK *mainWidget = new QRK;
+    QRK *mainWidget = new QRK;
 
-  bool dbSelect = false;
+    bool dbSelect = false;
 
-  foreach(const QString &arg, QApplication::arguments())
-  {
-    if ( arg[0] != '-' )
-      continue;
-
-    if ( arg == "-db" )
-      dbSelect = true;
-    else if ( (arg == "--fullscreen") || (arg == "-f") )
+    foreach(const QString &arg, QApplication::arguments())
     {
-      mainWidget->setWindowState(mainWidget->windowState() ^ Qt::WindowFullScreen);
+        if ( arg[0] != '-' )
+            continue;
+
+        if ( arg == "-db" )
+            dbSelect = true;
+        else if ( (arg == "--fullscreen") || (arg == "-f") )
+        {
+            mainWidget->setWindowState(mainWidget->windowState() ^ Qt::WindowFullScreen);
+        }
+        else if ( (arg == "--noPrinter") || (arg == "-n") )
+        {
+            mainWidget->setNoPrinter();
+        }
+        else // if ( arg == "--help" )  // --help or everything else we do not understand
+        {
+            printHelp();
+            return 0;
+        }
     }
-    else if ( (arg == "--noPrinter") || (arg == "-n") )
-    {
-      mainWidget->setNoPrinter();
-    }
-    else // if ( arg == "--help" )  // --help or everything else we do not understand
-    {
-      printHelp();
-      return 0;
-    }
-  }
 
-  // Global
-  mainWidget->statusBar()->setStyleSheet(
-        "QStatusBar::item { border: 1px solid lightgrey; border-radius: 3px;} "
-        );
+    // Global
+    mainWidget->statusBar()->setStyleSheet(
+                "QStatusBar::item { border: 1px solid lightgrey; border-radius: 3px;} "
+                );
 
-  mainWidget->show();
+    mainWidget->show();
 
-  if ( !Database::open( dbSelect) )
-    return 0;
+    if ( !Database::open( dbSelect) )
+        return 0;
 
-  /*check if we have SET Demomode*/
-  if (DemoMode::isModeNotSet()) {
-    QMessageBox messageBox(QMessageBox::Question,
-                QObject::tr("DEMOMODUS"),
-                QObject::tr("Wird QRK (Qt Registrier Kasse) im Echtbetrieb verwendet?\n\nJA wenn QRK im produktiven Einsatz ist.\n\nNEIN wenn DEMO Daten verwendet werden."),
-                QMessageBox::Yes | QMessageBox::No,
-                0);
+    /*check if we have SET Demomode*/
+    if (DemoMode::isModeNotSet()) {
+        QMessageBox messageBox(QMessageBox::Question,
+                               QObject::tr("DEMOMODUS"),
+                               QObject::tr("Wird QRK (Qt Registrier Kasse) im Echtbetrieb verwendet?\n\nJA wenn QRK im produktiven Einsatz ist.\n\nNEIN wenn DEMO Daten verwendet werden."),
+                               QMessageBox::Yes | QMessageBox::No,
+                               0);
         messageBox.setButtonText(QMessageBox::Yes, QObject::tr("Ja"));
         messageBox.setButtonText(QMessageBox::No, QObject::tr("Nein"));
 
-    if (messageBox.exec() == QMessageBox::Yes )
-    {
-      DemoMode::leaveDemoMode();
+        if (messageBox.exec() == QMessageBox::Yes )
+        {
+            DemoMode::leaveDemoMode();
+        }
     }
-  }
 
-  /*Check for MasterData*/
-  QString cri = Database::getCashRegisterId();
-  if ( cri.isEmpty() ) {
-    QMessageBox::warning(0, QObject::tr("Kassenidentifikationsnummer"), QObject::tr("Stammdaten müssen vollständig ausgefüllt werden.."));
-    QSettings settings(QSettings::IniFormat, QSettings::UserScope, "QRK", "QRK");
-    SettingsDialog *tab = new SettingsDialog(settings);
-    if (tab->exec() == QDialog::Accepted )
-    {
-      if ( Database::getCashRegisterId().isEmpty() )
-        return 0;
+    /*Check for MasterData*/
+    QString cri = Database::getCashRegisterId();
+    if ( cri.isEmpty() ) {
+        QMessageBox::warning(0, QObject::tr("Kassenidentifikationsnummer"), QObject::tr("Stammdaten müssen vollständig ausgefüllt werden.."));
+        QSettings settings(QSettings::IniFormat, QSettings::UserScope, "QRK", "QRK");
+        SettingsDialog *tab = new SettingsDialog(settings);
+        if (tab->exec() == QDialog::Accepted )
+        {
+            if ( Database::getCashRegisterId().isEmpty() )
+                return 0;
+        }
     }
-  }
 
-  QString title = QString("QRK V%1.%2 - Qt Registrier Kasse - %3").arg(QRK_VERSION_MAJOR).arg(QRK_VERSION_MINOR).arg(Database::getShopName());
-  mainWidget->currentRegisterYearLabel->setText(QObject::tr(" Kassenjahr: %1 ").arg(mainWidget->getCurrentRegisterYear()));
-  mainWidget->cashRegisterIdLabel->setText(QObject::tr(" Kassenidentifikationsnummer: %1 ").arg(Database::getCashRegisterId()));
-  mainWidget->setLastEOD(Reports::getLastEOD());
+    QString title = QString("QRK V%1.%2 - Qt Registrier Kasse - %3").arg(QRK_VERSION_MAJOR).arg(QRK_VERSION_MINOR).arg(Database::getShopName());
+    mainWidget->currentRegisterYearLabel->setText(QObject::tr(" Kassenjahr: %1 ").arg(mainWidget->getCurrentRegisterYear()));
+    mainWidget->cashRegisterIdLabel->setText(QObject::tr(" Kassenidentifikationsnummer: %1 ").arg(Database::getCashRegisterId()));
+    mainWidget->setLastEOD(Reports::getLastEOD());
 
-  mainWidget->setShopName();
-  mainWidget->setWindowTitle ( title );
-  mainWidget->init();
+    mainWidget->setShopName();
+    mainWidget->setWindowTitle ( title );
+    mainWidget->init();
 
 
-  signal(SIGTERM, sighandler);
+    signal(SIGTERM, sighandler);
 
-  return app.exec();
+    return app.exec();
 }
